31feda16439ecba358b96eb4526cbc64a9c9fb2a
taivv18 <taivv18@gmail.com>
2026-05-31 00:56:35 +0700

    exercise: completed task multiplication table

--- exercises/Module_1/07_geometry/src/source.c
+++ exercises/Module_1/07_geometry/src/source.c
@@ -1,9 +1,20 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <math.h>
 #include "source.h"
 
 void multi_table(unsigned int xsize, unsigned int ysize)
 {
+	for (size_t i = 1; i <= ysize; ++i) {
+		for (size_t j = i; j < xsize + i; ++j) {
+			if (i == 1 || j == i) {
+				printf("%zu ", j);
+			} else {
+				printf("%zu ", j + i - 1);
+			}
+		}
+		printf("\n");
+	}
 }
 
 void draw_triangle(unsigned int size)
